da098dd1cde6fb5dad0c549f03fe32b8180eda33
experience247-maker <experiencedemaghesti@gmail.com>
2025-04-18 13:36:01 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 require('dotenv').config(); 
 
-global.owner = process.env.OWNER_NUMBER; 
+global.owner = process.env.OWNER_NUMBER; 233501879278 
 global.anitav4 = process.env.BOT_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.ownername = process.env.OWNER_NAME || "David Cyril";
+global.ownername = process.env.OWNER_NAME || "Dadzie Ernest";
 global.packname = process.env.PACK_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
 global.author = process.env.AUTHOR || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
 global.channelname = process.env.CHANNEL_NAME || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
